ef817b0cae7a91ae7e53f3f26d19d3ea315e6a7f
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2023-03-23 14:21:10 +0300

    profile changed

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -10,17 +10,17 @@ export default function Profile() {
             <p className={profile.text}>Basic Information</p>{" "}
             <div className={profile.cardText}>
               <div className={profile.cardTextA}>
-                <p>Doğum tarihi</p>
-                <p>İkamet Şehri</p>
-                <p>Eğitim Durumu</p>
-                <p className={profile.span1}>Tercih Ettiği Rol</p>
+                <p>Date of birth</p>
+                <p>City</p>
+                <p>Education</p>
+                <p className={profile.span1}>Role</p>
               </div>
               <div className={profile.cardTextB}>
                 <p>06.10.1994</p>
                 <p>Erzurum</p>
                 <p className={profile.span2}>
                   {" "}
-                  Atatürk Ünv. Kentsel Tasarım Y.Lisans, 2023
+                  Atatürk Unv. Kentsel Tasarım Y.Lisans, 2023
                 </p>
                 <p>Frontend, UI, Backend</p>
               </div>
@@ -31,11 +31,15 @@ export default function Profile() {
           <div className={profile.abountText}>
             <p className={profile.aboutTextA}>About me</p>
             <p className={profile.aboutTextB}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
-              aut, odit laborum aliquam voluptatum nisi mollitia.
+              I can handle complex projects with ease, thanks to my strong
+              problem-solving skills and experience in various programming
+              languages. My passion for technology pushes me to stay up to date
+              with industry trends and constantly improve my skills.
               <br />
-              <br /> Mnima accusamus ratione soluta aperiam sit voluptate? Dicta
-              quod deserunt quam temporibus cumque magnam!
+              <br />I am also a team player and enjoy collaborating with others
+              to bring creative solutions to the table. Whether working
+              independently or as part of a team, I am committed to delivering
+              high-quality results on time.
             </p>
           </div>
         </section>
